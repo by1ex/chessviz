c597268e7128b707035d25f391c3e9263f833d46
Злобин Алексей Валерьевич <zlobin.aleksei95@gmail.com>
2019-03-20 10:15:59 +0700

    Added output function board

--- chessviz.cpp
+++ chessviz.cpp
@@ -1,5 +1,18 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <iostream>
+
+using namespace std;
+
+void output_board(char chess_board[8][8])
+{
+    int i, j;
+    for (i = 7; i > -1; i--) {
+        cout << i + 1 << " ";
+        for (j = 0; j < 8; j++)
+            cout << chess_board[i][j] << " ";
+        cout << endl;
+    }
+    cout << "  ";
+}
 
 int main()
 {
@@ -11,5 +24,6 @@ int main()
                               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                               {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                               {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+    output_board(chess_board);
     return 0;
 }
