fd2ddea4a7ef0ea4aae621924c270829f830f56a
Злобин Алексей Валерьевич <zlobin.aleksei95@gmail.com>
2019-05-01 21:24:14 +0700

    Record board status in html

--- src/board_print_html.cpp
+++ src/board_print_html.cpp
@@ -17,6 +17,44 @@ void printHTML(char chess_board[][8], char str_info[])
             fprintf(html_write,
                     "\n"
                     R"(<td>)");
+            if (chess_board[i][j] != ' ') {
+                if (chess_board[i][j] == 'r')
+                    fprintf(html_write,
+                            R"(<span class="black rook"></span>)");
+                if (chess_board[i][j] == 'n')
+                    fprintf(html_write,
+                            R"(<span class="black knight"></span>)");
+                if (chess_board[i][j] == 'b')
+                    fprintf(html_write,
+                            R"(<span class="black bishop"></span>)");
+                if (chess_board[i][j] == 'q')
+                    fprintf(html_write,
+                            R"(<span class="black queen"></span>)");
+                if (chess_board[i][j] == 'k')
+                    fprintf(html_write,
+                            R"(<span class="black king"></span>)");
+                if (chess_board[i][j] == 'p')
+                    fprintf(html_write,
+                            R"(<span class="black pawn"></span>)");
+                if (chess_board[i][j] == 'R')
+                    fprintf(html_write,
+                            R"(<span class="white rook"></span>)");
+                if (chess_board[i][j] == 'N')
+                    fprintf(html_write,
+                            R"(<span class="white knight"></span>)");
+                if (chess_board[i][j] == 'B')
+                    fprintf(html_write,
+                            R"(<span class="white bishop"></span>)");
+                if (chess_board[i][j] == 'Q')
+                    fprintf(html_write,
+                            R"(<span class="white queen"></span>)");
+                if (chess_board[i][j] == 'K')
+                    fprintf(html_write,
+                            R"(<span class="white king"></span>)");
+                if (chess_board[i][j] == 'P')
+                    fprintf(html_write,
+                            R"(<span class="white pawn"></span>)");
+            }
             fprintf(html_write, R"(</td>)");
         }
         fprintf(html_write, R"(</tr>)");
