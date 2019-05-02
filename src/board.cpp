b31f3ccffc376809c5dc03554c8e8c0459bcb700
Злобин Алексей Валерьевич <zlobin.aleksei95@gmail.com>
2019-05-02 16:47:08 +0700

    Add html open function to check moves

--- src/board.cpp
+++ src/board.cpp
@@ -3,4 +3,13 @@
 
 void checkSteps(char* txt, char chess_board[][8])
 {
+    printf("\n");
+    FILE* input_file;
+    input_file = fopen(txt, "a+");
+    if (input_file == NULL) {
+        printf("Cannot open file\n");
+    }
+    rewind(input_file);
+    while (!feof(input_file)) {
+    }
 }
