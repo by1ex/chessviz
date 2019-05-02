d9118c84e2ac4da5d243c4efec37d1a93e05fc08
Злобин Алексей Валерьевич <zlobin.aleksei95@gmail.com>
2019-05-02 17:09:49 +0700

    Output method change from text to html

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,6 @@
 
 using namespace std;
 
-void output_board(char chess_board[8][8])
-{
-    int i, j;
-    for (i = 7; i > -1; i--) {
-        cout << i + 1 << " ";
-        for (j = 0; j < 8; j++)
-            cout << chess_board[i][j] << " ";
-        cout << endl;
-    }
-    cout << "  ";
-    for (i = 97; i < 105; i++)
-        cout << (char)i << " ";
-    cout << endl;
-}
-
 int main()
 {
     char chess_board[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
@@ -30,6 +15,56 @@ int main()
                               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                               {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                               {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
-    output_board(chess_board);
+    FILE* output_html;
+    output_html = fopen("chessviz.html", "w+");
+    fprintf(output_html, R"( <html>
+        <head>
+        <title>Chessviz</title>
+        <style type="text/css">
+        table.chessboard {
+          border: 5px solid #333;
+          border-collapse: collapse;
+          height: 320px;
+          margin: 20px;
+          width: 320px;
+        }
+        table.chessboard caption {
+          text-align: left;
+        }
+        table.chessboard td {
+          background-color: #fff;
+          font-size: 25px;
+          height: 40px;
+          text-align: center;
+          vertical-align: middle;
+          width: 40px;
+        }
+        table.chessboard tr:nth-child(odd) td:nth-child(even),
+        table.chessboard tr:nth-child(even) td:nth-child(odd) {
+          background-color: #999;
+        }
+        table.chessboard .white.king:before   { content: "\2654"; }
+        table.chessboard .white.queen:before  { content: "\2655"; }
+        table.chessboard .white.rook:before   { content: "\2656"; }
+        table.chessboard .white.bishop:before { content: "\2657"; }
+        table.chessboard .white.knight:before { content: "\2658"; }
+        table.chessboard .white.pawn:before   { content: "\2659"; }
+        table.chessboard .black.king:before   { content: "\265A"; }
+        table.chessboard .black.queen:before  { content: "\265B"; }
+        table.chessboard .black.rook:before   { content: "\265C"; }
+        table.chessboard .black.bishop:before { content: "\265D"; }
+        table.chessboard .black.knight:before { content: "\265E"; }
+        table.chessboard .black.pawn:before   { content: "\265F"; }
+      </style>
+        </head>
+        <body>)");
+    fclose(output_html);
+    char str_start[30] = "0. start_position";
+    printHTML(chess_board, str_start);
+    output_html = fopen("chessviz.html", "a+");
+    fprintf(output_html, R"(</body>
+        </html>)");
+    fclose(output_html);
+    printf("HTML file created!");
     return 0;
 }
