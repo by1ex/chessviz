49f9fd941ee3e790bab6645a228b153b71c95d9e
Злобин Алексей Валерьевич <zlobin.aleksei95@gmail.com>
2019-05-02 22:04:36 +0700

    Сode formatting with .clang-format

--- src/board_print_html.cpp
+++ src/board_print_html.cpp
@@ -19,41 +19,29 @@ void printHTML(char chess_board[][8], char str_info[])
                     R"(<td>)");
             if (chess_board[i][j] != ' ') {
                 if (chess_board[i][j] == 'r')
-                    fprintf(html_write,
-                            R"(<span class="black rook"></span>)");
+                    fprintf(html_write, R"(<span class="black rook"></span>)");
                 if (chess_board[i][j] == 'n')
-                    fprintf(html_write,
-                            R"(<span class="black knight"></span>)");
+                    fprintf(html_write, R"(<span class="black knight"></span>)");
                 if (chess_board[i][j] == 'b')
-                    fprintf(html_write,
-                            R"(<span class="black bishop"></span>)");
+                    fprintf(html_write, R"(<span class="black bishop"></span>)");
                 if (chess_board[i][j] == 'q')
-                    fprintf(html_write,
-                            R"(<span class="black queen"></span>)");
+                    fprintf(html_write, R"(<span class="black queen"></span>)");
                 if (chess_board[i][j] == 'k')
-                    fprintf(html_write,
-                            R"(<span class="black king"></span>)");
+                    fprintf(html_write, R"(<span class="black king"></span>)");
                 if (chess_board[i][j] == 'p')
-                    fprintf(html_write,
-                            R"(<span class="black pawn"></span>)");
+                    fprintf(html_write, R"(<span class="black pawn"></span>)");
                 if (chess_board[i][j] == 'R')
-                    fprintf(html_write,
-                            R"(<span class="white rook"></span>)");
+                    fprintf(html_write, R"(<span class="white rook"></span>)");
                 if (chess_board[i][j] == 'N')
-                    fprintf(html_write,
-                            R"(<span class="white knight"></span>)");
+                    fprintf(html_write, R"(<span class="white knight"></span>)");
                 if (chess_board[i][j] == 'B')
-                    fprintf(html_write,
-                            R"(<span class="white bishop"></span>)");
+                    fprintf(html_write, R"(<span class="white bishop"></span>)");
                 if (chess_board[i][j] == 'Q')
-                    fprintf(html_write,
-                            R"(<span class="white queen"></span>)");
+                    fprintf(html_write, R"(<span class="white queen"></span>)");
                 if (chess_board[i][j] == 'K')
-                    fprintf(html_write,
-                            R"(<span class="white king"></span>)");
+                    fprintf(html_write, R"(<span class="white king"></span>)");
                 if (chess_board[i][j] == 'P')
-                    fprintf(html_write,
-                            R"(<span class="white pawn"></span>)");
+                    fprintf(html_write, R"(<span class="white pawn"></span>)");
             }
             fprintf(html_write, R"(</td>)");
         }
